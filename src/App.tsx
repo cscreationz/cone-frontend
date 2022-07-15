04d23f83d44d21102f7fbb28f55a71d3cf8d2e39
Akhil <akhillav@gmail.com>
2022-07-15 22:00:10 +0530

    set up user login pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route, Link } from "react-router-dom"
 import './App.css';
+import LandingPage from './pages/LandingPage/LandingPage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
+import OnboardingPage from './pages/OnboardingPage/OnboardingPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register/" element={<RegisterPage />} />
+        <Route path="/onboard/:id" element={<OnboardingPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+export default function RegisterPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  return (
+    <div>
+      <h1>Register</h1>
+
+      <form action="" onSubmit={handleSubmit}>
+        <label htmlFor="">Username:</label>
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
+
+        <label htmlFor="">Password:</label>
+        <input type="password" name="" id="" value={password} onChange={(e) => setPassword(e.target.value)}/>
+
+        <button type="submit">Register</button>
+      </form>
+     
+    </div>
+  )
+}
